ec8776c0cb6237db5469704ad359bad58f159078
vexel <vldmrpashkin@gmail.com>
2026-01-30 02:39:33 +0300

    Fix: build vercel

--- app/dashboard/customers/page.tsx
+++ app/dashboard/customers/page.tsx
@@ -5,9 +5,9 @@ import { Metadata } from 'next'
 
 export const metadata: Metadata = { title: 'Customers' }
 
-export default async function Page({ searchParams }: { searchParams?: { query?: string } }) {
-  const query = searchParams?.query ?? ''
-  const customers = await fetchFilteredCustomers(query)
+export default async function Page() {
+
+  const customers = await fetchFilteredCustomers('')
 
   return (
     <div>
