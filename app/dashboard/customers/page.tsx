e32718e678a30c8ea91d7199ff6fe7d3a23e7be9
vexel <vldmrpashkin@gmail.com>
2026-01-30 02:34:42 +0300

    finish

--- app/dashboard/customers/page.tsx
+++ app/dashboard/customers/page.tsx
@@ -1,3 +1,17 @@
-export default function Page() {
-  return <p>Customers Page</p>
+// app/customers/page.tsx
+import CustomersTable from '@/app/ui/customers/table'
+import { fetchFilteredCustomers } from '@/app/lib/data'
+import { Metadata } from 'next'
+
+export const metadata: Metadata = { title: 'Customers' }
+
+export default async function Page({ searchParams }: { searchParams?: { query?: string } }) {
+  const query = searchParams?.query ?? ''
+  const customers = await fetchFilteredCustomers(query)
+
+  return (
+    <div>
+      <CustomersTable customers={customers} />
+    </div>
+  )
 }
--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,27 @@
 import '@/app/ui/global.css'
 import { inter } from '@/app/ui/fonts'
+import { Metadata } from 'next'
+
+const emojiFavicon = (emoji: string) =>
+  `data:image/svg+xml,` +
+  encodeURIComponent(
+    `<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 100 100">
+      <text y=".9em" font-size="90">${emoji}</text>
+    </svg>`,
+  )
+
+
+export const metadata: Metadata = {
+  title: {
+    template: '%s | Acme Dashboard',
+    default: 'Acme Dashboard',
+  },
+  icons: {
+    icon: emojiFavicon('🌐'),
+  },
+  description: 'The official Next.js Learn Dashboard built with App Router.',
+  metadataBase: new URL('https://next-learn-dashboard.vercel.sh'),
+}
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
