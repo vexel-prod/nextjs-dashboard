6fc1a548b9659e29e12440fc728cb2d33c65a429
vexel <vldmrpashkin@gmail.com>
2026-01-30 02:46:39 +0300

    Fix: builds errors

--- app/dashboard/customers/page.tsx
+++ app/dashboard/customers/page.tsx
@@ -1,17 +1,18 @@
-// app/customers/page.tsx
 import CustomersTable from '@/app/ui/customers/table'
 import { fetchFilteredCustomers } from '@/app/lib/data'
 import { Metadata } from 'next'
+import { Suspense } from 'react'
 
 export const metadata: Metadata = { title: 'Customers' }
 
 export default async function Page() {
-
   const customers = await fetchFilteredCustomers('')
 
   return (
     <div>
-      <CustomersTable customers={customers} />
+      <Suspense fallback={null}>
+        <CustomersTable customers={customers} />
+      </Suspense>
     </div>
   )
 }
