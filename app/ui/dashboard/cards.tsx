8087d3baa9a33191bb112a45a102ddbdcf51fd31
vexel <vldmrpashkin@gmail.com>
2026-01-29 23:44:52 +0300

    feat: add environment variable for next-auth

--- app/ui/dashboard/cards.tsx
+++ app/ui/dashboard/cards.tsx
@@ -1,33 +1,41 @@
-import {
-  BanknotesIcon,
-  ClockIcon,
-  UserGroupIcon,
-  InboxIcon,
-} from '@heroicons/react/24/outline';
-import { lusitana } from '@/app/ui/fonts';
+import { BanknotesIcon, ClockIcon, UserGroupIcon, InboxIcon } from '@heroicons/react/24/outline'
+import { lusitana } from '@/app/ui/fonts'
+import { fetchCardData } from '@/app/lib/data'
 
 const iconMap = {
   collected: BanknotesIcon,
   customers: UserGroupIcon,
   pending: ClockIcon,
   invoices: InboxIcon,
-};
+}
 
 export default async function CardWrapper() {
+  const { numberOfInvoices, numberOfCustomers, totalPaidInvoices, totalPendingInvoices } =
+    await fetchCardData()
   return (
     <>
-      {/* NOTE: Uncomment this code in Chapter 9 */}
-
-      {/* <Card title="Collected" value={totalPaidInvoices} type="collected" />
-      <Card title="Pending" value={totalPendingInvoices} type="pending" />
-      <Card title="Total Invoices" value={numberOfInvoices} type="invoices" />
       <Card
-        title="Total Customers"
+        title='Collected'
+        value={totalPaidInvoices}
+        type='collected'
+      />
+      <Card
+        title='Pending'
+        value={totalPendingInvoices}
+        type='pending'
+      />
+      <Card
+        title='Total Invoices'
+        value={numberOfInvoices}
+        type='invoices'
+      />
+      <Card
+        title='Total Customers'
         value={numberOfCustomers}
-        type="customers"
-      /> */}
+        type='customers'
+      />
     </>
-  );
+  )
 }
 
 export function Card({
@@ -35,17 +43,17 @@ export function Card({
   value,
   type,
 }: {
-  title: string;
-  value: number | string;
-  type: 'invoices' | 'customers' | 'pending' | 'collected';
+  title: string
+  value: number | string
+  type: 'invoices' | 'customers' | 'pending' | 'collected'
 }) {
-  const Icon = iconMap[type];
+  const Icon = iconMap[type]
 
   return (
-    <div className="rounded-xl bg-gray-50 p-2 shadow-sm">
-      <div className="flex p-4">
-        {Icon ? <Icon className="h-5 w-5 text-gray-700" /> : null}
-        <h3 className="ml-2 text-sm font-medium">{title}</h3>
+    <div className='rounded-xl bg-gray-50 p-2 shadow-sm'>
+      <div className='flex p-4'>
+        {Icon ? <Icon className='h-5 w-5 text-gray-700' /> : null}
+        <h3 className='ml-2 text-sm font-medium'>{title}</h3>
       </div>
       <p
         className={`${lusitana.className}
@@ -54,5 +62,5 @@ export function Card({
         {value}
       </p>
     </div>
-  );
+  )
 }
